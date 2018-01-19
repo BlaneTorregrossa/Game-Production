e1a00eb417172c9ec90b2823428fab894e48394c
BlaneTorregrossa <blanet97@cox.net>
2018-01-19 15:53:16 -0600

    Proto Camera Start; Not Good

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Character : ScriptableObject
+{
+    [SerializeField] public Vector3 Position;
+    [SerializeField] public float Health;
+}
