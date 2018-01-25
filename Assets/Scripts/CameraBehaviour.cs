63da96ea6cfb5787756e2a3f3d0cab0130a1f798
BlaneTorregrossa <blanet97@cox.net>
2018-01-25 17:55:17 -0600

    Camera changes + CineMachine Still broke

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+
+    public float zoom;
+
+    public GameObject CharacterA;
+    public GameObject CharacterB;
+
+    public GameObject camCenter;
+
+
+    private Quaternion camRotation = new Quaternion();
+    private float zoomlock;
+
+    void Start()
+    {
+        transform.rotation = new Quaternion();
+    }
+
+    void Update()
+    {
+        CamZoom();
+    }
+
+    public void CamZoom()
+    {
+        #region W/O CineMachine
+        //if (camCenter.transform.position.x >= 0)
+        //{
+        //    Quaternion camRotation = new Quaternion();
+        //    camRotation.eulerAngles = new Vector3(45, 0, 0);
+
+        //    transform.rotation = camRotation;
+        //}
+
+        //if (camCenter.transform.position.x <= 0)
+        //{
+        //    Quaternion camRotation = new Quaternion();
+        //    camRotation.eulerAngles = new Vector3(45, 180, 0);
+
+        //    transform.rotation = camRotation;
+        //}
+
+        #endregion
+
+        // Need this in to work with cineMachine
+        zoom = Vector3.Distance(CharacterA.transform.position, CharacterB.transform.position);
+
+        camRotation.eulerAngles = new Vector3(35, 0, 0);
+        transform.rotation = camRotation;
+        transform.position = new Vector3(camCenter.transform.position.x, transform.position.y + zoom, camCenter.transform.position.z);
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerCenterBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCenterBehaviour : MonoBehaviour
+{
+
+    public GameObject CharacterA;
+    public GameObject CharacterB;
+
+    public float zoomScale;
+
+    private Vector3 objectPosDelta;
+
+    void Update()
+    {
+        SetCenter();
+    }
+
+    public Vector3 SetCenter()
+    {
+
+        // To get the center of the two players
+        if (Vector3.Distance(CharacterA.transform.position, CharacterB.transform.position) > 1)
+        {
+            objectPosDelta = CharacterB.transform.position + (CharacterA.transform.position - CharacterB.transform.position) / 2;   // Center area
+        }
+
+        zoomScale = Vector3.Distance(CharacterA.transform.position, CharacterB.transform.position);
+        transform.position = objectPosDelta + new Vector3(0 , zoomScale / 2, 0);
+        return objectPosDelta;
+    }
+}
