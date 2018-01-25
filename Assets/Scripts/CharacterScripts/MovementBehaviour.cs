b70c6f97b9cc2b4ded57e8f1f988c7e4e0774638
Christopher Walle <cjwalle97@gmail.com>
2018-01-25 17:56:08 -0600

    Update
    Update

---
+++ Assets/Scripts/CharacterScripts/MovementBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBehaviour : MonoBehaviour
+{
+    public float speed;
+    public bool dash;
+
+    private Vector3 _direction;
+    private float _acceleration;
+    private float _velocity;
+
+    // Use this for initialization
+    void Start()
+    {
+        speed = 0.2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _direction.x = Input.GetAxis("Horizontal") * speed;
+        _direction.z = Input.GetAxis("Vertical") * speed;
+        if (Input.GetKeyDown("space"))
+        {
+            dash = true;
+        }
+        BasicMove(_direction);
+
+    }
+
+    void BasicMove(Vector3 d)
+    {
+        if (dash)
+        {
+            
+        }
+        else
+        {
+            
+        }
+    }
+}
