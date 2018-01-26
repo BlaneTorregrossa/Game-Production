4c5777e03e5be7a259aa387875e61fe80605666e
Christopher Walle <cjwalle97@gmail.com>
2018-01-26 16:06:44 -0600

    Update
    Update

--- Assets/Scripts/CharacterScripts/CharacterController.cs
+++ Assets/Scripts/CharacterScripts/CharacterController.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterControls : MonoBehaviour {
-
+public class CharacterController : MonoBehaviour
+{
+    public Character CharacterConfig;
+    
+    
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+    
 }
