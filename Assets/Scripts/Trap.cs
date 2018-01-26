caca72b2e935526ae3146972d27a179b07f4c0e9
BlaneTorregrossa <blanet97@cox.net>
2018-01-26 11:20:26 -0600

    .

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : ScriptableObject
+{
+    public float trapDamage;
+    public float delay;
+}
---
+++ Assets/Scripts/TrapBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapBehaviour : MonoBehaviour
+{
+
+    public Trap currentTrap = new Trap();
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void ActivateTrap()
+    {
+        if (currentTrap.delay <= 0)
+        {
+            
+        }
+    }
+
+    public void DealDamage()
+    {
+        //player health - currentTrap.trapDamage;
+    }
+
+    public void SetPlayerDamage()
+    {
+
+    }
+}
