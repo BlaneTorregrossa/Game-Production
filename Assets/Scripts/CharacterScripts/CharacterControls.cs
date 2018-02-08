ee99deb7564fe06d6a479644470b813fb4f97337
Christopher Walle <cjwalle97@gmail.com>
2018-02-08 17:34:53 -0600

    Update
    Update

--- Assets/Scripts/CharacterScripts/CharacterControls.cs
+++ Assets/Scripts/CharacterScripts/CharacterControls.cs
@@ -9,21 +9,26 @@ public class CharacterControls : ScriptableObject
     public MovementBehaviour movement;
     public int gamePadNum;
     public Vector3 moveDirection;
+    public Vector3 dashDirection;
     public Vector3 lookDirection;
     
     public void Move()
     {
-        var x = Input.GetAxis("LeftStickX") * characterConfig.Speed;
-        var z = Input.GetAxis("LeftStickY") * characterConfig.Speed;
+        var x = Input.GetAxis("Horizontal") * characterConfig.Speed;
+        var z = Input.GetAxis("Vertical") * characterConfig.Speed;
+        moveDirection = new Vector3(x, 0, z);
     }
 
     public void Dash()
     {
-
+        var x = Input.GetAxis("Horizontal") * characterConfig.DashSpeed;
+        var z = Input.GetAxis("Vertical") * characterConfig.DashSpeed;
     }
 
     public void Look()
     {
-
+        var x = Input.GetAxis("LookHorizontal");
+        var z = Input.GetAxis("LookVertical");
+        lookDirection = new Vector3(x, 0, z);
     }
 }
