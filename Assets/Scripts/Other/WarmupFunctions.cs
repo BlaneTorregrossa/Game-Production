7be849ac184612a6dd106d9d3b32052f95cff076
Christopher Walle <cjwalle97@gmail.com>
2018-02-08 09:33:50 -0600

    Update
    Update

---
+++ Assets/Scripts/Other/WarmupFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarmupFunctions : MonoBehaviour
+{
+    public List<GameObject> objects;
+
+    private GameObject _object;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void addobjecttoList(GameObject g)
+    {
+        objects.Add(g);
+        Debug.Log("Object was added")
+    }
+}
