4f845c7dafda3724c4aaf173a6a0ab7549717d5d
Matthew Williamson <chuwilliamson@gmail.com>
2018-03-15 13:34:46 -0500

    adding the list and changing health

--- Assets/Scripts/CharacterScripts/Character.cs
+++ Assets/Scripts/CharacterScripts/Character.cs
@@ -6,12 +6,18 @@ using UnityEngine;
 [CreateAssetMenu(menuName ="Character")]
 public class Character : ScriptableObject
 {
+    public List<Part> parts;
+    void OnEnable()
+    {
+        parts = new List<Part>() { Left, Right, LegSet, HeadPiece };
+    }
+
     public Part Left;
     public Part Right;
     public Part LegSet;
     public Part HeadPiece;
     public string Name;
-    public int Heatlh;
+    public int Health;
     public int DashCharges;
     public float Speed;
     public float DashSpeed;
