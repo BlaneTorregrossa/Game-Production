6e110fa3911bb9c31a325bc8745a45d13069746c
Matthew Williamson <chuwilliamson@gmail.com>
2018-03-15 13:39:02 -0500

    add the functions to the globalgamemanager

--- Assets/Scripts/Other/Chris's Lecture assignments/GlobalGameManager.cs
+++ Assets/Scripts/Other/Chris's Lecture assignments/GlobalGameManager.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 [CreateAssetMenu(menuName = "GlobalGameManager")]
 public class GlobalGameManager : ScriptableObject
 {
     public void PrintInfo(string i)
     {
         Debug.Log(i);
+    }
+    public void GoToScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void GoToScene(int index)
+    {
+        SceneManager.LoadScene(index);
     }
 }
