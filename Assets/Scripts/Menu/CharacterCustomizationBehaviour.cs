a32e5f11ed89f9ba303cd10693d9d581a983d6c8
BlaneTorregrossa <blanet97@cox.net>
2018-03-15 09:57:13 -0500

    3/15/2017 10:00 AM

--- Assets/Scripts/Menu/CharacterCustomizationBehaviour.cs
+++ Assets/Scripts/Menu/CharacterCustomizationBehaviour.cs
@@ -9,8 +9,8 @@ using UnityEngine.SceneManagement;
 
 public class CharacterCustomizationBehaviour : MonoBehaviour
 {
-    public static SetUpCharacterBehaviour SetupInstance;    //  Instance of character setup
-
+    [HideInInspector]
+    public SetUpCharacterBehaviour SetupInstance;    //  Instance of character setup
     //  Text objects for updating menu information
     #region TextBoxes
     [HideInInspector]
@@ -42,12 +42,9 @@ public class CharacterCustomizationBehaviour : MonoBehaviour
     void Start()
     {
         SetupInstance = transform.gameObject.AddComponent<SetUpCharacterBehaviour>();  // adding component for setting up the character
-        
-        #region Character
         CustomizedCharacter = ScriptableObject.CreateInstance<Character>(); // Changing character object name 
         CustomizedCharacter.name = "Character A";   //  Object Name
         CustomizedCharacter.Name = "Character A";   //  Object variable for Name
-        #endregion
 
         #region SortingTroughLists
         LeftArmNum = 0;
@@ -79,93 +76,6 @@ public class CharacterCustomizationBehaviour : MonoBehaviour
         SetupInstance.SetArmRight = UnlockedRightArms[RightArmNum];     //  Gives selected part to setup instance
         SetupInstance.SetLegs = UnlockedLegs[LegsNum];  //  Gives selected part to setup instance
         SetupInstance.SetHead = UnlockedHeads[HeadNum]; //  Gives selected part to setup instance
-    }
-
-    // After new changes this function might be useless ***
-    // Current Issues: Repeating (sort of), Needs to be shorter ***
-    // For saving parts as Scriptable Objects to be used in a new scene
-    public void SaveParts(List<Part> currentParts)
-    {
-        for (int i = 0; i < currentParts.Count; i++)
-        {
-            Part selectedPart = currentParts[i];
-
-            // Shorten This ***
-            if (selectedPart.partType == SetUpCharacterBehaviour.RobotParts.HEAD)
-            {
-                var selectedHead = ScriptableObject.CreateInstance<Head>();
-                selectedHead.partName = SetupInstance.SetHead.partName; //  keep?
-                selectedHead.description = SetupInstance.SetHead.description;   //  keep?
-                selectedHead.partPos = SetupInstance.SetHead.partPos;   //  keep?
-                selectedHead.partType = SetupInstance.SetHead.partType;
-                selectedHead.prefab = SetupInstance.SetHead.prefab;
-                selectedHead.name = "Saved Head";
-
-                SetupInstance.RobotPartList[i] = selectedHead;
-            }
-
-            // Shorten this ***
-            else if (selectedPart.partType == SetUpCharacterBehaviour.RobotParts.LEFTARM)
-            {
-                var selectedLeft = ScriptableObject.CreateInstance<Arm>();
-                selectedLeft.partName = SetupInstance.SetArmLeft.partName;  //  keep?
-                selectedLeft.description = SetupInstance.SetArmLeft.description;    //  keep?
-                selectedLeft.partPos = SetupInstance.SetArmLeft.partPos;    //  keep?
-                selectedLeft.partType = SetupInstance.SetArmLeft.partType;
-                selectedLeft.prefab = SetupInstance.SetArmLeft.prefab;
-                selectedLeft.isLeft = true;
-                selectedLeft.isRight = false;
-                selectedLeft.isMelee = SetupInstance.SetArmLeft.isMelee;
-                selectedLeft.isRanged = SetupInstance.SetArmLeft.isRanged;
-                selectedLeft.meleeAttackSpeed = SetupInstance.SetArmLeft.meleeAttackSpeed;
-                selectedLeft.meleeDamage = SetupInstance.SetArmLeft.meleeDamage;
-                selectedLeft.projectileDamage = SetupInstance.SetArmLeft.projectileDamage;
-                selectedLeft.projectileSpeed = SetupInstance.SetArmLeft.projectileSpeed;
-                selectedLeft.name = "Saved Left Arm";
-
-                SetupInstance.RobotPartList[i] = selectedLeft;
-            }
-
-            // Shorten This ***
-            else if (selectedPart.partType == SetUpCharacterBehaviour.RobotParts.LEGS)
-            {
-                var selectedLegs = ScriptableObject.CreateInstance<Legs>();
-                selectedLegs.partName = SetupInstance.SetLegs.partName; //  keep?
-                selectedLegs.description = SetupInstance.SetLegs.description;   //  keep?
-                selectedLegs.partPos = SetupInstance.SetLegs.partPos;   //  keep?
-                selectedLegs.partType = SetupInstance.SetLegs.partType;
-                selectedLegs.prefab = SetupInstance.SetLegs.prefab;
-                selectedLegs.DashCharges = SetupInstance.SetLegs.DashCharges;
-                selectedLegs.DashSpeed = SetupInstance.SetLegs.DashSpeed;
-                selectedLegs.Speed = SetupInstance.SetLegs.Speed;
-                selectedLegs.name = "Saved Legs";
-
-                SetupInstance.RobotPartList[i] = selectedLegs;
-            }
-
-            // Shorten This ***
-            else if (selectedPart.partType == SetUpCharacterBehaviour.RobotParts.RIGHTARM)
-            {
-                var selectedRight = ScriptableObject.CreateInstance<Arm>();
-                selectedRight.partName = SetupInstance.SetArmRight.partName;    //  keep?
-                selectedRight.description = SetupInstance.SetArmRight.description;  //  keep?
-                selectedRight.partPos = SetupInstance.SetArmRight.partPos;  //  keep?
-                selectedRight.partType = SetupInstance.SetArmRight.partType;
-                selectedRight.prefab = SetupInstance.SetArmRight.prefab;
-                selectedRight.isLeft = false;
-                selectedRight.isRight = true;
-                selectedRight.isMelee = SetupInstance.SetArmRight.isMelee;
-                selectedRight.isRanged = SetupInstance.SetArmRight.isRanged;
-                selectedRight.meleeAttackSpeed = SetupInstance.SetArmRight.meleeAttackSpeed;
-                selectedRight.meleeDamage = SetupInstance.SetArmRight.meleeDamage;
-                selectedRight.projectileDamage = SetupInstance.SetArmRight.projectileDamage;
-                selectedRight.projectileSpeed = SetupInstance.SetArmRight.projectileSpeed;
-                selectedRight.name = "Saved Right Arm";
-
-                SetupInstance.RobotPartList[i] = selectedRight;
-            }
-
-        }
     }
 
     //  Buttons for switching parts + changing description
@@ -286,10 +196,14 @@ public class CharacterCustomizationBehaviour : MonoBehaviour
     // For bringing the character given to the "target range" area
     public void BringToTargetRange()
     {
-        SaveParts(SetupInstance.RobotPartList); // Calls function to save parts for scene transition
+        DontDestroyOnLoad(SetupInstance.CurrentCharacter);
         SceneManager.LoadScene("1.TargetRange");    // Switches to named scene
         //  NOTE: Anything called after will be applied to this scene and not the new scene.
         SceneManager.UnloadSceneAsync("257.CharacterSelectTest");   //  Unloads (resets) named scene
+
+        //  ------------------------Destroy Later-------------------------------
+        SceneManager.LoadScene("257.CharacterSelectTest");
+        SceneManager.UnloadSceneAsync("1.TargetRange");
     }
     #endregion
 }
