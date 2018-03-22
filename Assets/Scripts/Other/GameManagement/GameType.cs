029adb54d9fd9efee38f9354932c1749d9d0e0cf
BlaneTorregrossa <blanet97@cox.net>
2018-03-22 13:52:12 -0500

    3/22/2018 1:20

---
+++ Assets/Scripts/Other/GameManagement/GameType.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GameType")]
+public class GameType : ScriptableObject
+{
+
+    public enum GameMode
+    {
+        MENU = 0,
+        TARGETRANGE = 1,
+        PVP = 2,
+    }
+
+    public GameMode Mode;
+}
--- Assets/Scripts/Other/GameManagement/TargetRangeBehaviour.cs
+++ Assets/Scripts/Other/GameManagement/TargetRangeBehaviour.cs
@@ -1,17 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+// Simple Check Function for if Player or all targets are "Dead " in scene to swich from this scene and back
 public class TargetRangeBehaviour : MonoBehaviour
 {
 
-    public int Time;
+    public GlobalGameManager GameManagerInstance;
+    public GameType CurrentGameMode;
+    public CharacterBehaviour Character;
     public List<CharacterBehaviour> Targets;
-    public bool isRange;
 
-	void Start ()
+    public GameObject UIChange;
+    public GameObject CharacterChange;
+
+    void Start()
     {
-		
-	}
-	
+        UIChange.SetActive(false);
+        CharacterChange.SetActive(true);
+
+        //  Check for Correct Scene Type. If wrong type, switches back to Character Select Menu
+        if (CurrentGameMode.Mode != GameType.GameMode.TARGETRANGE)
+        {
+            Debug.Log("Incorrect Game Mode set for this scene. What is given: " + CurrentGameMode.Mode);
+            GameManagerInstance.GoToScene("257.CharacterSelectTest");
+        }
+    }
+    
+    void Update()
+    {
+        //  Dsiplay buttons if player or all targets are dead, Removes Players and Targets
+        if (Targets[0].isDead == true && Targets[1].isDead == true && Targets[2].isDead == true || Character.isDead == true)
+        {
+            UIChange.SetActive(true);
+            CharacterChange.SetActive(false);
+        }
+    }
+
+
 }
