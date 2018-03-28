97d2cf5efbbb35720d3eac58b49613e2de359108
BlaneTorregrossa <blanet97@cox.net>
2018-03-28 17:55:08 -0500

    Round Setup  with need changes

--- Assets/Scripts/Other/GameManagement/Round.cs
+++ Assets/Scripts/Other/GameManagement/Round.cs
@@ -2,8 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Commented code is what was attempted before. DO NOT REPEAT!
 public class Round : ScriptableObject
 {
-    public bool Won;
-    public bool Tie;
+
+    //public enum RoundResult //  Result for each Round
+    //{
+    //    UNDECIDED = 0,
+    //    WON = 1,
+    //    LOSS = 2,
+    //    DRAW = 3,
+    //}
+
+    //public RoundResult Result;
+
+    //public bool Won;    // Check if Player won given Round
+    //public bool Loss;   // Check if Player Loss given Round
+    //public bool Draw;    // Check if Player tied given Round
 }
