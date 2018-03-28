7beba625aa84068340f17a5321a6023f0f9e2cf7
BlaneTorregrossa <blanet97@cox.net>
2018-03-28 17:54:41 -0500

    comments

--- Assets/Scripts/Other/GameManagement/GameType.cs
+++ Assets/Scripts/Other/GameManagement/GameType.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// For determining set gamemode of selected scene
 [CreateAssetMenu(menuName = "GameType")]
 public class GameType : ScriptableObject
 {
 
     public enum GameMode
     {
-        MENU = 0,
-        TARGETRANGE = 1,
-        PVP = 2,
+        MENU = 0,           //  Main Menu
+        TARGETRANGE = 1,    //  Single Playable Character Range
+        PVP = 2,            //  For Character Vs Character Scene
     }
 
-    public GameMode Mode;
+    public GameMode Mode;   //  If this sciptable object is made into it's own asset    (Not Recomended)
 }
