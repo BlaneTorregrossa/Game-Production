d05cce6091ef57bffc3bbc1c105114283b92c6cb
BlaneTorregrossa <blanet97@cox.net>
2018-03-28 17:54:22 -0500

    Cleanup + minor changes

--- Assets/Scripts/Other/GameManagement/GameLoopBehaviour.cs
+++ Assets/Scripts/Other/GameManagement/GameLoopBehaviour.cs
@@ -3,113 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-// Simple Check Function for if Player or all targets are "Dead " in scene to swich from this scene and back
 public class GameLoopBehaviour : MonoBehaviour
 {
+    public GameType.GameMode CurrentGameMode;   //  Current Game mode for given scene
+    public CharacterBehaviour PlayerCharacter;   //  Character Behaviour for Player
+    public CharacterBehaviour OpponentCharacter; //  Character Behaviour for Opponent
+    public List<CharacterBehaviour> TargetCharacters;    //  List of targets in the Target Range
 
-    public GameType.GameMode CurrentGameMode;
-    public CharacterBehaviour Player;
-    public CharacterBehaviour Opponent;
-    public List<CharacterBehaviour> Targets;
-    public GameObject UIChange;
-    public GameObject CharacterChange;
-    public int RoundTime;
-    public int RoundMax;
-
-    [SerializeField]
-    private Text Timer;
-    private bool Paused;
-    private bool StillAlive;
-    [SerializeField]
-    private List<Round> PlayerRounds;
-    [SerializeField]
-    private List<Round> OpponentRounds;
+    private bool Paused;    //  For determining if game is paused
+    [SerializeField] private int RoundMax;   //  Max amount of rounds for the match. Might need to move to the Round Scriptable Object.
+    private Round CurrentRound;   //  Round Scriptable Object being used    ***
+    [SerializeField] private float SetTime;  //  Starting time for round
+    [SerializeField] private float RoundTime;    //  Time for each Round
+    [SerializeField] private float PausedTime;   //  Time while paused
+    [SerializeField] private GameObject MenuUI; //  Menu based UI that is made avalible once certain conditions are met
+    [SerializeField] private GameObject CharacterChange;  //   Character objects in the scene
 
     void Start()
     {
-        #region Target Range SetUp
+        #region Target Range Start
+        //  Disables Pause menu, enable characters
         if (CurrentGameMode == GameType.GameMode.TARGETRANGE)
         {
-            UIChange.SetActive(false);
+            MenuUI.SetActive(false);
             CharacterChange.SetActive(true);
         }
         #endregion
 
-        #region PVP SetUp
-        // Set Up rounds
+        #region PVP Start
         if (CurrentGameMode == GameType.GameMode.PVP)
         {
-            for (int i = 0; i < RoundMax; i++)
-            {
-                Round newRound = new Round();
-                newRound.Tie = false;
-                newRound.Won = false;
-                newRound.name = "Round " + (i + 1);
-                PlayerRounds.Add(newRound);
-                OpponentRounds.Add(newRound);
-            }
+            MenuUI.SetActive(false);    //  Temporary   ***
+            CharacterChange.SetActive(true);    //  Temporary   ***
         }
         #endregion
-
-        StillAlive = true;
     }
 
     void Update()
     {
-        // Call for checking if all targets are dead    ***
-        if (CurrentGameMode == GameType.GameMode.TARGETRANGE && CheckTargets(Targets) == false)
-        {
-            UIChange.SetActive(true);
-            CharacterChange.SetActive(false);
-        }
-
+        //  In place of the lack of a pause button being set on the controller
+        //  NOTE:  Remove once Controller is able to call pause function
         #region Temp Pause Menu Input
-        // Controller not set to call function yet
-        if (Input.GetKeyDown(KeyCode.Escape) && CurrentGameMode == GameType.GameMode.TARGETRANGE)
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             EnablePause(CurrentGameMode);
         }
         #endregion
-    }
 
-    // Target Range: Check if all targets are dead so menu can be brought up    ***
-    public bool CheckTargets(List<CharacterBehaviour> targets)
-    {
-        for (int i = 0; i < Targets.Count; i++)
+        // ISSUE: Timer could potentially be set in a seperate behaviour script  ***
+        #region Timer
+        //  Timer for when the game isn't paused. Does not tick down while game is paused.
+        //  Rt = (St - T) - Pt
+        if (Paused == false && CurrentGameMode == GameType.GameMode.PVP)
         {
-            if (Targets[i].isDead == true || Player.isDead == true)
-            {
-                if (Targets[i].isDead == false && Player.isDead == false)
-                {
-                    StillAlive = true;
-                    return StillAlive;
-                }
-                StillAlive = false;
-            }
+            RoundTime = (SetTime - Time.timeSinceLevelLoad) - PausedTime;
         }
-        return StillAlive;
+
+        //  To not track time while game is "paused".
+        //  Pt = (St - T) - Rt
+        if (Paused == true && CurrentGameMode == GameType.GameMode.PVP)
+        {
+            PausedTime = (SetTime - Time.timeSinceLevelLoad) - RoundTime;
+        }
+        #endregion        
     }
 
-    // Need to add: Check for PVP and menu scene and how said pause menus are brought into scene.
+    //  Very Temporary  ***
     public void EnablePause(GameType.GameMode mode)
     {
-        if (mode == GameType.GameMode.TARGETRANGE)
+        // Enables pause menu
+        if (Paused == false)
         {
-            // Enables pause menu
-            if (Paused == false && StillAlive == true)
-            {
-                UIChange.SetActive(true);
-                CharacterChange.SetActive(false);
-                Paused = true;
-            }
+            MenuUI.SetActive(true);
+            CharacterChange.SetActive(false);
+            Paused = true;
+        }
 
-            // Disables pause menu
-            else if (Paused == true && StillAlive == true)
-            {
-                UIChange.SetActive(false);
-                CharacterChange.SetActive(true);
-                Paused = false;
-            }
+        // Disables pause menu
+        else if (Paused == true)
+        {
+            MenuUI.SetActive(false);
+            CharacterChange.SetActive(true);
+            Paused = false;
         }
     }
 }
