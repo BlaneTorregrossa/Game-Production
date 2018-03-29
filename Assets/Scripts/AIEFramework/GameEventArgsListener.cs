1c07e2095cf7b13324e47611a921cacc45ad17cf
Christopher Walle <cjwalle97@gmail.com>
2018-03-29 17:52:10 -0500

    AIEFramework
    add aie framework

---
+++ Assets/Scripts/AIEFramework/GameEventArgsListener.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+namespace  AIEFramework
+{
+
+    public class GameEventArgsListener : MonoBehaviour
+    {
+        public GameEventArgs Event;
+        public Object Sender;
+        public GameEventArgsResponse Response;
+
+        private void OnEnable()
+        {
+            Event.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            Event.UnregisterListener(this);
+        }
+
+        public void OnEventRaised(Object[] args)
+        {
+            if (Sender == null || Sender == args[0])
+                Response.Invoke(args);
+        }
+    }
+
+}
---
+++ Assets/Scripts/AIEFramework/InteractableBehaviour.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+namespace AIEFramework
+{
+
+    public interface IInteractionSetHandler
+    {
+        void OnInteractionSet(Object[] args);
+    }
+
+    public interface IInteractionReleaseHandler
+    {
+        void OnInteractionRelease(Object[] args);
+    }
+
+    public interface IInteractionBeginHandler
+    {
+        void OnInteractionBegin(Object[] args);
+    }
+
+    public interface IInteractionEndHandler
+    {
+        void OnInteractionEnd(Object[] args);
+    }
+
+    public interface IPhysicsTriggerEnterHandler
+    {
+        void OnPhysicsTriggerEnter(Object[] args);
+    }
+
+    public interface IPhysicsTriggerExitHandler
+    {
+        void OnPhysicsTriggerExit(Object[] args);
+    }
+
+    [RequireComponent(typeof(PhysicsTriggerListener))]
+    public class InteractableBehaviour : MonoBehaviour, IInteractable, IPhysicsTriggerEnterHandler, IPhysicsTriggerExitHandler
+    {
+        public IInteractor interactor;
+        public GameObject interactorGameObject;
+        public GameEventArgsResponse Response;
+        public GameEventArgs InteractionSet;
+        public GameEventArgs InteractionBegin;
+        public GameEventArgs InteractionEnd;
+        public GameEventArgs InteractionReleased;
+
+        public void Interact(object token)
+        {
+            InteractableBeginInteraction(token as GameObject);
+        }
+
+        public void InteractableBeginInteraction(Object token)
+        {
+            InteractionBegin.Raise(token);
+            Response.Invoke(new Object[] { this, interactorGameObject, token });
+        }
+
+        public void InteractableEndInteraction()
+        {
+            InteractionEnd.Raise(gameObject, interactorGameObject);
+        }
+
+        public void InteractableReleaseInteraction()
+        {
+            InteractionReleased.Raise(gameObject, interactorGameObject);
+        }
+
+        public void OnPhysicsTriggerEnter(Object[] args)
+        {
+            var sender = args[0] as GameObject;
+            var actor = args[1] as GameObject;
+            if (sender == null)
+                return;
+
+            interactorGameObject = actor;
+            interactor = interactorGameObject.GetComponent<IInteractor>();
+            interactor.Interaction_Set(this);
+            InteractionSet.Raise(gameObject, actor);
+        }
+
+        public void OnPhysicsTriggerExit(Object[] args)
+        {
+            var sender = args[0] as GameObject;
+            var actor = args[1] as GameObject;
+            if (sender == null)
+                return;
+
+            interactor.Interaction_Release(this);
+            interactorGameObject = null;
+            interactor = null;
+            InteractionReleased.Raise(gameObject, actor);
+        }
+    }
+
+}
