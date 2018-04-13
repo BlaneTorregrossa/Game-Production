dd038291096045e96b3dcdc566bed9e4f982f9a7
Christopher Walle <cjwalle97@gmail.com>
2018-04-13 10:40:34 -0500

    Update #1 4/13/2018
    Got a proper gap for the bullets. MachineGun Fire is now complete

--- Assets/Scripts/CharacterParts/Character1/FireProjectileBehaviour.cs
+++ Assets/Scripts/CharacterParts/Character1/FireProjectileBehaviour.cs
@@ -9,28 +9,49 @@ public class FireProjectileBehaviour : MonoBehaviour
     public Transform location;
     public bool rapidFire;
 
+    private int _reloadTime;
+    private int _shotTime;
+
     [HideInInspector]
     public GameObject firedProjectile;
     
+    private void Start()
+    {
+        _reloadTime = 0;
+    }
+
     private void FixedUpdate()
     {
-        if(Input.GetKey("space") && rapidFire == true)
+        if (Input.GetKey("space") && rapidFire == true)
         {
+            _shotTime += 1;
             FireMachineGunBullet();
         }
-        if(Input.GetKey("space") && rapidFire == false)
+        if (Input.GetKey("space") && rapidFire == false)
         {
-            FireGrenade();
+            if (_reloadTime == 0)
+            {
+                FireGrenade();
+                _reloadTime = 100;
+            }
+        }
+        if(_reloadTime > 0)
+        {
+            _reloadTime -= 1;
         }
     }
 
     public void FireMachineGunBullet()
     {
-        firedProjectile = Instantiate(projectileObject, location.position, transform.rotation);
-        firedProjectile.transform.forward = transform.forward;
-        firedProjectile.GetComponent<ProjectileBehavior>().SetOwner(characterConfig);
-        firedProjectile.GetComponent<Rigidbody>().velocity += gameObject.transform.forward * firedProjectile.GetComponent<MachineGunBulletBehaviour>().BulletConfig.speed;
-        Destroy(firedProjectile, 2f);
+        if (_shotTime == 5)
+        {
+            firedProjectile = Instantiate(projectileObject, location.position, transform.rotation);
+            firedProjectile.transform.forward = transform.forward;
+            firedProjectile.GetComponent<ProjectileBehavior>().SetOwner(characterConfig);
+            firedProjectile.GetComponent<Rigidbody>().velocity += gameObject.transform.forward * firedProjectile.GetComponent<MachineGunBulletBehaviour>().BulletConfig.speed;
+            Destroy(firedProjectile, 2f);
+            _shotTime = 0;
+        }
     }
 
     public void FireGrenade()
