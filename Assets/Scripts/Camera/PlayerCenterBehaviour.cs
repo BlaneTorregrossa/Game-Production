cd3d776fd236f4c74824c152f7c14e3cac00213a
BlaneTorregrossa <blanet97@cox.net>
2018-04-25 12:30:37 -0500

    Comments

--- Assets/Scripts/Camera/PlayerCenterBehaviour.cs
+++ Assets/Scripts/Camera/PlayerCenterBehaviour.cs
@@ -8,7 +8,7 @@ public class PlayerCenterBehaviour : MonoBehaviour
 {
     public Transform TargetA;  //  Object A
     public Transform TargetB;  //  Object B
-    public Transform Camera; //  
+    public Transform Camera; //  Camera transform
 
     public float CharacterToCharacterDistance;      //  Distance of Both Objects from each other
     public float CharacterAToCameraDistance;    //  Distance from a character to a camera
@@ -18,20 +18,20 @@ public class PlayerCenterBehaviour : MonoBehaviour
 
     void Update()
     {
-        SetCenter();
-        CharacterToCharacterDistance = Vector3.Distance(TargetA.transform.position, TargetB.transform.position);
-        CharacterAToCameraDistance = Vector3.Distance(TargetA.transform.position, Camera.transform.position);
-        CharacterBToCameraDistacne = Vector3.Distance(TargetB.transform.position, Camera.transform.position);
+        SetCenter();    //  Calling funbction that sets this objects's position
+        CharacterToCharacterDistance = Vector3.Distance(TargetA.transform.position, TargetB.transform.position);    //  Give distance between the targets
+        CharacterAToCameraDistance = Vector3.Distance(TargetA.transform.position, Camera.transform.position);   //  Give distance between TargetA and the camera
+        CharacterBToCameraDistacne = Vector3.Distance(TargetB.transform.position, Camera.transform.position);   //  Give distance between TargetB and the camera
     }
 
-    //  To set the position in the wrold of this gameobject between two other given objects
+    //  To set the position in the world of this gameobject between two other given objects
     public void SetCenter()
     {
         // To get the center of the two objects
         if (Vector3.Distance(TargetA.transform.position, TargetB.transform.position) > 0.5f)
         {
-            objectPosDelta = TargetB.transform.position + (TargetA.transform.position - TargetB.transform.position) / 2;   // Center area
-            transform.position = objectPosDelta;
+            objectPosDelta = TargetB.transform.position + (TargetA.transform.position - TargetB.transform.position) / 2;   // Defining center area
+            transform.position = objectPosDelta;    //  Set this objects position to the difference between the two objects
         }
         else
             return;
--- Assets/Scripts/Camera/VirtualCameraViewBehaviour.cs
+++ Assets/Scripts/Camera/VirtualCameraViewBehaviour.cs
@@ -7,34 +7,35 @@ public class VirtualCameraViewBehaviour : MonoBehaviour
 {
 
     [SerializeField]
-    private Transform CameraPos;
+    private Transform CameraPos;    //  Camera Transform
     [SerializeField]
-    private GameObject CenterObject;
-    private PlayerCenterBehaviour Center;
+    private GameObject CenterObject;    //  Used for getting the PlayerCenterBehaviourComponent information
+    private PlayerCenterBehaviour Center;   //  Used for getting Center information
     [SerializeField]
-    private Vector3 CamBoundries;
+    private Vector3 CamBoundries;   //  Boundries for the Camera Movement
     [SerializeField]
-    private float CamYModifier, CamZModifier;
+    private float CamYModifier, CamZModifier;   //  Moddifiers for the how the camera moves position on the Y or Z axis
     [SerializeField]
-    private float CloseViewY, CloseViewZ;
+    private float CloseViewY, CloseViewZ;   //  Transform values for the camera when set in static position for x and y axis
     [SerializeField]
-    private float CameraMovementUp;
-    private float YMovement, ZMovement;
+    private float CameraMovementUp; //  Modifier for how camera moves "upwards" in the world
+    [SerializeField]
+    private float CharacterDistanceThreshold;   //  Threshold for how close the characters can be
+    private float YMovement, ZMovement; //  Axis movement for the camera's position
 
     void Start()
     {
-        Center = CenterObject.GetComponent<PlayerCenterBehaviour>();
+        Center = CenterObject.GetComponent<PlayerCenterBehaviour>();    //  Assigning PlayerCenterComponent object
     }
 
     //  For updating Camrea Position Object
     void Update()
     {
-        transform.position = CameraPositioning();
+        transform.position = CameraPositioning();   //  Setting camera's Current Position in the world
         transform.position = BoundryCheck();   //  Function to control if camera is in it's current set bounds
     }
 
-    //  Set Boundry for camera follow object on x y and z axis
-    //  Can be simplified   *
+    //  Set movement boundry for camera follow object on x y and z axis
     public Vector3 BoundryCheck()
     {
         Vector3 ReturnVector;
@@ -42,41 +43,45 @@ public class VirtualCameraViewBehaviour : MonoBehaviour
         float NewPosY = transform.position.y;
         float NewPosZ = transform.position.z;
 
+        //  X axis boundry check
         if (transform.position.x > CamBoundries.x)
             NewPosX = CamBoundries.x;
         else if (transform.position.x < -CamBoundries.x)
             NewPosX = -CamBoundries.x;
 
+        //  y axis boundry check
         if (transform.position.y > CamBoundries.y)
             NewPosY = CamBoundries.y;
         else if (transform.position.y < -CamBoundries.y)
             NewPosY = -CamBoundries.y;
 
+        //  z axis boundry check
         if (transform.position.z > CamBoundries.z)
             NewPosZ = CamBoundries.z;
         else if (transform.position.z < -CamBoundries.z)
             NewPosZ = -CamBoundries.z;
 
-        return ReturnVector = new Vector3(NewPosX, NewPosY, NewPosZ);
+        return ReturnVector = new Vector3(NewPosX, NewPosY, NewPosZ);   //  position returned to camera transform object
     }
 
-    //  Issue: If CameraMovementUp is set above a certain value, one of the characters can go offscreen
-    //  Causes unnessecary rotation + can be improved   ***
+    //  Returns specific position for the camera to move to
     public Vector3 CameraPositioning()
     {
-        Vector3 ReturnVector = transform.position;
+        Vector3 ReturnVector = transform.position;  //  What will be returned
 
-        YMovement = Center.CharacterToCharacterDistance / CamYModifier;
-        ZMovement = -Center.CharacterToCharacterDistance / CamZModifier;
+        YMovement = Center.CharacterToCharacterDistance / CamYModifier; //  position based on distanceand modifier for the y axis for the camera
+        ZMovement = -Center.CharacterToCharacterDistance / CamZModifier;    //  position based on distanceand modifier for the z axis for the camera
 
-        if (Center.CharacterToCharacterDistance > 10)
+        //  If the distance of both targets exceeeds the given value then the camera adjusts to keep both characters in view
+        if (Center.CharacterToCharacterDistance > CharacterDistanceThreshold)
             ReturnVector = new Vector3(Center.transform.position.x,
                 Center.CharacterToCharacterDistance * (YMovement / CameraMovementUp),
                 Center.CharacterToCharacterDistance * (ZMovement));
+        //  else the camera is in a semi-lcoked position that can see both targets
         else
             ReturnVector = new Vector3(CenterObject.transform.position.x, CloseViewY, CloseViewZ);
 
-        return ReturnVector;
+        return ReturnVector;    //  Return camera camera position
     }
 
 }
