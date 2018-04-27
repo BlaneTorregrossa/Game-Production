a651905b97743471dea631df86e07ffc8de0efdd
BlaneTorregrossa <blanet97@cox.net>
2018-04-27 13:37:32 -0500

    Small Setup 4/27/2018 1:37 P.M.

---
+++ Assets/Scripts/Other/GameManagement/AnimationObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationObject : ScriptableObject
+{
+    public enum AnimationStates
+    {
+        Idle = 0,
+        Aim = 1,
+        Fire = 2,
+        Melee = 3,
+        Special = 4,
+        Walk = 5,
+        Hurt = 6,
+        Death = 7,
+    }
+
+    public string AnimationName;
+    public string AnimationDescription;
+    public Animation CurrentAnimation;
+    public AnimationStates State;
+}
---
+++ Assets/Scripts/Other/GameManagement/AnimatorControllerBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorControllerBehaviour : MonoBehaviour
+{
+
+    public AnimationObject CurrentState;
+    public AnimationObject PreviousState;
+    public AnimationObject NextState;
+    public GameObject CurrentObject;
+
+    [SerializeField]
+    private Animator CurrentAnimator;
+    [SerializeField]
+    private Animation CurrentAnnimation;
+
+    void Start()
+    {
+        CurrentAnimator = CurrentObject.GetComponent<Animator>();
+        CurrentAnnimation = new Animation();
+    }
+
+    //  For Switching Animations based on states
+    public AnimationObject SwitchToNextAnimation(AnimationObject NewState)
+    {
+        NextState.State = NewState.State;
+        PreviousState.State = CurrentState.State;
+        CurrentState.State = NextState.State;
+
+        return CurrentState;
+    }
+
+}
