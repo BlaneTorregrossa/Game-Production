e56b125b42389412217e9ccf12deee7bcf47b82c
BlaneTorregrossa <blanet97@cox.net>
2018-05-11 06:52:24 -0500

    Health, SkyBox and UI assets added

---
+++ Assets/Scripts/CharacterScripts/HealthFetchBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthFetchBehaviour : MonoBehaviour
+{
+
+    public CharacterBehaviour CurrentCharacter;
+
+    //  Change Health Bar based on remaining character Health
+    void Update()
+    {
+        
+    }
+}
