ff1b70b9534f6f2d6c683a0da52cf69e0613cc92
Christopher Walle <cjwalle97@gmail.com>
2018-05-18 11:33:11 -0500

    Update # 1 5/18/2018
    Update contains new interface IBreakable as well as Barrel and Crate
    Scriptable Objects as the Behaviours that use them

---
+++ Assets/Scripts/Other/Enviornment/Barrel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrel : ScriptableObject, IDamageable, IDamager
+{
+    public GameObject ExplosionObject;
+    public float Damage;
+    public float Durrability;
+
+    public void DoDamage(IDamageable damageable)
+    {
+        damageable.TakeDamage(Damage);
+    }
+
+    public void TakeDamage(float amount)
+    {
+        Durrability -= amount;
+    }
+    
+}
---
+++ Assets/Scripts/Other/Enviornment/Crate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate : ScriptableObject, IDamageable
+{
+    public float Durability;
+    
+    public void TakeDamage(float amount)
+    {
+        Durability -= amount;
+    }
+}
